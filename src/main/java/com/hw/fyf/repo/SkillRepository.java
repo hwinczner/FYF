f5c569bdef4dfb1c81e9a89c2feb70cc35c3c64c
Henry <henrywinczner@gmail.com>
2026-07-06 14:21:37 -0400

    added skills CRUD repo

---
+++ src/main/java/com/hw/fyf/repo/SkillRepository.java
@@ -0,0 +1,9 @@
+package com.hw.fyf.repo;
+
+import com.hw.fyf.models.Skill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface SkillRepository extends JpaRepository<Skill, UUID> {
+}
